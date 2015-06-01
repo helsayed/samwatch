38baba15725ebe11ba39b7f2fa194ca873330e5f
Mahmoud Abdelaziz <mahmoud.abdelaziz@badrit.com>
2015-06-01 11:42:21 +0200

    change the js library

--- app/assets/javascripts/badrSalesman.js
+++ app/assets/javascripts/badrSalesman.js
@@ -1,89 +1,153 @@
-function BadrSalesmanActivity(type){
-	this.type = type;
-	this.item_id = 0;
-	this.item_price = 0;
-	this.rate = 0;
-  this.cookieUserId = '0';
-	this.userId = '0';
-	this.retailerToken = '55b21feecb7562083395d320b226f77f'
-	this.specifyRemoteUrl();
+base_url = "http://badrsalesman:3000";
+
+function identifyUser(){
+  if ($.cookie("user_identity")){
+    userId = $.cookie("user_identity");
+  }else{
+    userId = Math.round(new Date().getTime() + (Math.random() * 100));
+    $.cookie("user_identity", userId);
+  }
+  return userId;
 }
 
-BadrSalesmanActivity.prototype.identifyUser = function(){
-	  if ($.cookie("user_identity")){
-      cUserId = $.cookie("user_identity");
-    }else{
-      cUserId = Math.round(new Date().getTime() + (Math.random() * 100));
-      $.cookie("user_identity", cUserId);
+function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
+function initCookies(){
+  window.openTime = new Date();
+
+  if ($.cookie("products")){
+    if ($.cookie("products").indexOf($('meta[name=item_id]').attr('content')) < 0){
+      $.cookie("products", $.cookie("products") +','+  $('meta[name=item_id]').attr('content'), {expires: new Date($.cookie("products_expiration"))});
     }
-    this.cookieUserId = cUserId;
+  }else{
+    var date = new Date();
+    date.setTime(date.getTime() + (15 * 60 * 1000));
+    $.cookie("products_expiration", date);
+    $.cookie("products", $('meta[name=item_id]').attr('content'), { expires: date});
+  }
+}
+
+var BadrSalesmanActivity = function(access_token, options){
+  this.item_id = 0;
+  this.item_price = 0;
+  this.rate = 0;
+  this.cookieUserId = identifyUser();
+  if(typeof options['user_id'] === undefined){
     if ($('meta[name=user_id]').length > 0 ){
       this.userId = $('meta[name=user_id]').attr('content')
     }
+  }else{
+    this.userId = options['user_id']
+  }
+
+  if(typeof options['item_id'] === undefined){
+    if ($('meta[name=item_id]').length > 0 ){
+      this.itemId = $('meta[name=item_id]').attr('content')
+    }
+  }else{
+    this.itemId = options['item_id']
+  }
+
+
+  this.retailerToken = access_token;
+
+  this.url = base_url + '/api/v1/activities/log_activity';
+  this.activity_types = ["view", "order", "purchase", "rate"];
+}
+
+BadrSalesmanActivity.prototype.sendActivity = function(activity_type){
+  if(this.activity_types.indexOf(activity_type) > -1){ // Valid activity type
+    $.ajax({
+      type: 'POST',
+      url: this.url ,
+      data:{
+        access_token: this.retailerToken,
+        activities:{
+          activity_type: activity_type,
+          item_id: this.itemId ,
+          price: $('meta[name=price]').attr('content') ,
+          rating_value: $('.rate').attr('data') ,
+          session_time: window.timeDiff/100  //time in seconds
+        },
+        user_data: { user_cookie_id: this.cookieUserId, user_forign_id: this.userId}
+      },
+      crossDomain: true,
+      contentType: "application/json; charset=utf-8",
+      dataType: "jsonp",
+      async: false,
+      cache: false
+    });
+    
+  }
+
+};
+
+
+// -------------------------- Recommender ----------------------
+var BadrSalesmanRecommender = function(access_token, options){
+  initCookies();
+  this.item_ids = 0;
+  this.retailerToken = access_token;
+  this.itemId = options['item_id'];
+
+  this.cookieUserId = identifyUser();
+  if(typeof options['user_id'] === undefined){
+    if ($('meta[name=user_id]').length > 0 ){
+      this.userId = $('meta[name=user_id]').attr('content')
+    }
+  }else{
+    this.userId = options['user_id']
+  }
+
+  if(typeof options['item_id'] === undefined){
+    if ($('meta[name=item_id]').length > 0 ){
+      this.itemId = $('meta[name=item_id]').attr('content')
+    }
+  }else{
+    this.itemId = options['item_id']
+  }
+  
+
+  if ($('#badrSalesmanRecommendation').length > 0){
+    this.getRecommendations();
+  }
+  if ($('#badrSalesmanPromotion').length > 0){
+    this.getPromotions();
+  }
+
+  this.activity = new BadrSalesmanActivity(access_token, options);
+  var that_activity = this.activity
+  window.onbeforeunload = function(e){
+    window.timeDiff = (new Date) - window.openTime //time in milliseconds
+    that_activity.sendActivity('view');
+  };
 
 }
 
-BadrSalesmanActivity.prototype.specifyRemoteUrl = function(){
-		//TODO change this url to real machine 
-	this.url = 'http://salesman.badrit.com/api/v1/activities/log_activity';
-};
-
-BadrSalesmanActivity.prototype.sendActivity = function(){
-  this.identifyUser()
-	$.ajax({
-    type: 'POST',
-    url: this.url ,
-    data:{
-      access_token: this.retailerToken,
-      activities:{
-        activity_type: this.type,
-        item_id: $('meta[name=item_id]').attr('content') ,
-        price: $('meta[name=price]').attr('content') ,
-        rating_value: $('.rate').attr('data') ,
-        session_time: window.timeDiff/100  //time in seconds
-      },
-    	user_data: {
-      	user_cookie_id: this.cookieUserId,
-        user_forign_id: this.userId
-    	}
-    },
-    crossDomain: true,
-    contentType: "application/json; charset=utf-8",
-    dataType: "jsonp",
-    async: false,
-    cache: false
-  });
-};
-
-function BadrSalesmanRecommender(){
-	this.item_ids = 0;
-	this.cookieUserId = '0';
-  this.userId = '0';
-  this.retailerToken = "55b21feecb7562083395d320b226f77f"
+BadrSalesmanRecommender.prototype.sendActivity = function(type){
+  this.activity.sendActivity(type);
 }
 
 BadrSalesmanRecommender.prototype.getRecommendations = function(){
-	//TODO change this url to real machine 
-	this.url = 'http://salesman.badrit.com/api/v1/recommendations/get_recommendation';
-  this.identifyUser();
+  this.url = base_url + '/api/v1/recommendations/get_recommendation';
   if ($('meta[name=item_id]').length > 0 ){
-		recommendation_item_data = {
+    recommendation_item_data = {
       item_ids:  $.cookie("products"), //$('meta[name=item_id]').attr('content'),
       category_id: $('meta[name=category_id]').attr('content')
-		}
+    }
   }else{
-	recommendation_item_data = {}
+    recommendation_item_data = {}
   }
-	$.ajax({
+
+  $.ajax({
     type: 'GET',
     url: this.url ,
     data:{
       access_token: this.retailerToken,
-      recommendation_data:{recommendation_item_data: recommendation_item_data},
-    	user_data: {
-      	user_cookie_id: this.cookieUserId,
-        user_forign_id: this.userId
-    	}
+      recommendation_data: {recommendation_item_data: recommendation_item_data},
+      user_data: { user_cookie_id: this.cookieUserId, user_forign_id: this.userId}
     },
     crossDomain: true,
     contentType: "application/json; charset=utf-8",
@@ -93,36 +157,16 @@ BadrSalesmanRecommender.prototype.getRecommendations = function(){
     jsonpCallback: 'recommendationJsonParser'
   });
 };
-//ToDo remove redunduncy 
-BadrSalesmanRecommender.prototype.identifyUser = function(){
-	  if ($.cookie("user_identity")){
-      cUserId = $.cookie("user_identity");
-    }else{
-      cUserId = Math.round(new Date().getTime() + (Math.random() * 100));
-      $.cookie("user_identity", userId);
-    }
-    this.cookieUserId = cUserId;
-    if ($('meta[name=user_id]').length > 0 ){
-      this.userId = $('meta[name=user_id]').attr('content')
-    }
-
-}
-
 
 
 BadrSalesmanRecommender.prototype.getPromotions = function(){
-  //TODO change this url to real machine 
-  this.url = 'http://salesman.badrit.com/api/v1/promotions/get_promotions';
-  this.identifyUser();
+  this.url = base_url + '/api/v1/promotions/get_promotions';
   $.ajax({
     type: 'GET',
     url: this.url ,
     data:{
       access_token: this.retailerToken,
-      user_data: {
-        user_cookie_id: this.cookieUserId,
-        user_forign_id: this.userId
-      }
+      user_data: { user_cookie_id: this.cookieUserId, user_forign_id: this.userId }
     },
     crossDomain: true,
     contentType: "application/json; charset=utf-8",
@@ -132,21 +176,6 @@ BadrSalesmanRecommender.prototype.getPromotions = function(){
     jsonpCallback: 'promotionsJsonParser'
   });
 };
-//ToDo remove redunduncy 
-BadrSalesmanRecommender.prototype.identifyUser = function(){
-    if ($.cookie("user_identity")){
-      cUserId = $.cookie("user_identity");
-    }else{
-      cUserId = Math.round(new Date().getTime() + (Math.random() * 100));
-      $.cookie("user_identity", cUserId);
-    }
-    this.cookieUserId = cUserId;
-    if ($('meta[name=user_id]').length > 0 ){
-      this.userId = $('meta[name=user_id]').attr('content')
-    }
-
-}
-
 
 function promotionsJsonParser(promotionJson){
   //ToDo handle failur case
@@ -161,51 +190,22 @@ function promotionsJsonParser(promotionJson){
 }
 
 function recommendationJsonParser(recommendationJson){
-	//ToDo handle failur case
+  //ToDo handle failur case
   if (recommendationJson['Recommendations'].length > 0){
     $('#badrSalesmanRecommendation').append("<h4>Recommended Products for you </h4>");
 
     $.each(recommendationJson["Recommendations"], function(index, value){
-  		$('#badrSalesmanRecommendation').append("<div id='recommended_product' style='margin-bottom: 5px;width:150px; height:120px; float: left; margin-right: 10px; border: 1px solid gray; padding: 6px;'><a href=" + value[1]['url']+ ">	<div style = 'height:60px'><img style='width:135px;height:90px;' src=" + value[1]['image'] + "></div><label style='text-align: center;margin-top: 30px;text-overflow: ellipsis;width: 150px;overflow: hidden;white-space: nowrap;'>" + value[1]['title'] + "</label></a></div>");
-  	});
+      $('#badrSalesmanRecommendation').append("<div id='recommended_product' style='margin-bottom: 5px;width:150px; height:120px; float: left; margin-right: 10px; border: 1px solid gray; padding: 6px;'><a href=" + value[1]['url']+ "> <div style = 'height:60px'><img style='width:135px;height:90px;' src=" + value[1]['image'] + "></div><label style='text-align: center;margin-top: 30px;text-overflow: ellipsis;width: 150px;overflow: hidden;white-space: nowrap;'>" + value[1]['title'] + "</label></a></div>");
+    });
     $('#badrSalesmanRecommendation').attr('style',"border: 1px solid #BFBFBF; display: inline-block; padding: 14px;border-radius: 5px;margin-bottom: 5px;  width: 101%;");
   }
-	
+  
 }
-
-function round(value, decimals) {
-    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
-}
-// for view auto detection 
-
-window.onbeforeunload = function(e){
-	if ($('meta[name=activity]').length > 0 ){
-		window.timeDiff = (new Date) - window.openTime //time in milliseconds
-	  badrActivity = new BadrSalesmanActivity($('meta[name=activity]').attr('content'));
-  	badrActivity.sendActivity();
-
-	}
-};
 
 $( document ).ready(function(){
-	window.openTime = new Date();
+  access_token = "1c3d5a1b870cb66eabb05d501a701dfc";
+  var user_id = $('meta[name=user_id]').attr('content');
+  var item_id = $('meta[name=item_id]').attr('content');
 
-  if ($.cookie("products")){
-    if ($.cookie("products").indexOf($('meta[name=item_id]').attr('content')) < 0){
-      $.cookie("products", $.cookie("products") +','+  $('meta[name=item_id]').attr('content'), {expires: new Date($.cookie("products_expiration"))});
-    }
-  }else{
-    var date = new Date();
-    date.setTime(date.getTime() + (15 * 60 * 1000));
-    $.cookie("products_expiration", date);
-    $.cookie("products", $('meta[name=item_id]').attr('content'), { expires: date});
-  }
-	badrRecommender = new BadrSalesmanRecommender();
-  if ($('#badrSalesmanRecommendation').length > 0){
-		badrRecommender.getRecommendations();
-	}
-  if ($('#badrSalesmanPromotion').length > 0){
-    badrRecommender.getPromotions();
-  }
-
+  badrRecommender = new BadrSalesmanRecommender(access_token, {'user_id': user_id, 'item_id': item_id});
 });
